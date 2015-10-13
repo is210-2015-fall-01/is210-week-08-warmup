4f3624813d559d0bcd51dea8fed5d5d1afbdec32
Nick Ileczko <nickileczko@gmail.com>
2015-10-13 21:25:14 +0000

    Task4

--- task_04.py
+++ task_04.py
@@ -7,6 +7,9 @@ MAX_LENGTH = 80
 LONGSTR = 'short'
 
 # You code goes here
+LENGTH = len(MYINPUT)
+if LENGTH > MAX_LENGTH:
+    LONGSTR = 'long'
 
 OUTPUT = 'That certainly was a {} story!'.format(LONGSTR)
 print OUTPUT
