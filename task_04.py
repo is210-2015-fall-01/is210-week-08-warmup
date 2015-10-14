35411b414bb606dbb4ea98e069033c5b119265d4
Alex Doler <aedoler@gmail.com>
2015-10-14 22:14:44 +0000

    task 4

--- task_04.py
+++ task_04.py
@@ -6,7 +6,11 @@ MYINPUT = raw_input('Tell me a story! ')
 MAX_LENGTH = 80
 LONGSTR = 'short'
 
-# You code goes here
+INPUTLEN = len(MYINPUT)
+
+if INPUTLEN > MAX_LENGTH:
+    LONGSTR = 'long'
+
 
 OUTPUT = 'That certainly was a {} story!'.format(LONGSTR)
 print OUTPUT
