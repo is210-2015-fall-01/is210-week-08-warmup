c68d42c72461ad05539426198265d28b0bc5bbd0
Vagrant User <cj.fernanders1986@gmail.com>
2015-10-15 02:26:45 +0000

    Week 8 Warmup

--- task_04.py
+++ task_04.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""A single if statement."""
+"""if statement."""
 
 MYINPUT = raw_input('Tell me a story! ')
 MAX_LENGTH = 80
 LONGSTR = 'short'
+INPUT_LEN = len(MYINPUT)
 
-# You code goes here
+if INPUT_LEN > MAX_LENGTH:
+    LONGSTR = 'long'
 
 OUTPUT = 'That certainly was a {} story!'.format(LONGSTR)
 print OUTPUT
