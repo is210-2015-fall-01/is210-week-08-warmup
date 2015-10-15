28e71b06947a4d41eaab2dd5650eb8704bb86de4
Grace Park <pjeanne14@gmail.com>
2015-10-15 03:08:52 +0000

    All lint tests passed. The unit test have same error message of' cannot import module wraps. Google says to reinstall development environment... Try running tests on Github.

---
+++ task_02.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Extends raw_input() usage & coverts the collected user input's data type"""
+
+MY_INTEGER = int(raw_input('What is the answer to the life the universe and \
+                            everything? '))
--- task_04.py
+++ task_04.py
@@ -6,7 +6,9 @@ MYINPUT = raw_input('Tell me a story! ')
 MAX_LENGTH = 80
 LONGSTR = 'short'
 
-# You code goes here
+VAR1 = len(MYINPUT)
+if VAR1 > MAX_LENGTH:
+    LONGSTR = 'long'
 
 OUTPUT = 'That certainly was a {} story!'.format(LONGSTR)
 print OUTPUT
