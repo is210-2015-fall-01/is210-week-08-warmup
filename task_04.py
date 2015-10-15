a11c809729f8b3b34f5ec0e215fea3ab227d172f
Saulat Majid <solyraja@hotmail.com>
2015-10-15 17:19:05 +0000

    Submitting all warmup tasks.

--- task_04.py
+++ task_04.py
@@ -2,11 +2,17 @@
 # -*- coding: utf-8 -*-
 """A single if statement."""
 
+
 MYINPUT = raw_input('Tell me a story! ')
 MAX_LENGTH = 80
 LONGSTR = 'short'
 
-# You code goes here
+len(MYINPUT)
+if MYINPUT > MAX_LENGTH:
+    LONGSTR = 'long'
+else:
+    print LONGSTR
+
 
 OUTPUT = 'That certainly was a {} story!'.format(LONGSTR)
 print OUTPUT
