a10a62dbba41f63d9d0103fd40ea1c1f47d1e6fa
Nick Ileczko <nickileczko@gmail.com>
2015-10-15 17:05:02 +0000

    task5

--- task_05.py
+++ task_05.py
@@ -2,24 +2,23 @@
 # -*- coding: utf-8 -*-
 """Task 5 BP."""
 
-BLOODP = raw_input('What is your blood pressure reading?')
 
-BLOODP = int(BLOODP)
+BLOODP = int(raw_input('What is your blood pressure reading?'))
 
 if BLOODP <= 89:
-    BP_STATUS = 'Low.'
+    BP_STATUS = 'Low'
 
 elif BLOODP >= 90 and BLOODP <= 119:
-    BP_STATUS = 'Ideal.'
+    BP_STATUS = 'Ideal'
 
 elif BLOODP >= 120 and BLOODP <= 139:
-    BP_STATUS = 'Warning!'
+    BP_STATUS = 'Warning'
 
 elif BLOODP >= 140 and BLOODP <= 159:
-    BP_STATUS = 'High!'
+    BP_STATUS = 'High'
 
 else:
-    BP_STATUS = 'Emergency!'
+    BP_STATUS = 'Emergency'
 
 BPRESULT = 'Your status is currently: {}'.format(BP_STATUS)
 
