0f97bac7167ae6c3534acdbab0971c9dbea06937
Tresanne <tresannebonnick@hotmail.com>
2015-10-15 00:59:56 +0000

    Updates

--- task_05.py
+++ task_05.py
@@ -8,16 +8,16 @@ BP = (int(raw_input('What is your blood pressure? ')))
 if BP <= 89:
     BP_STATUS = 'Low'
 
-elif BP is 90 and BP <= 119:
+elif 90 <= BP <= 119:
     BP_STATUS = 'Ideal'
 
-elif BP is 120 and BP <= 139:
+elif 120 <= BP <= 139:
     BP_STATUS = 'Warning'
 
-elif BP is 140 and BP <= 159:
+elif 140 <= BP <= 159:
     BP_STATUS = 'High'
 
-elif BP >= 160:
+else:
     BP_STATUS = 'Emergency'
 
 NOTIFY = 'Your status is currently: {}'.format(BP_STATUS)
